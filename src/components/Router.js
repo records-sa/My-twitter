32c324ff0c21a9c1855c4378bb7bbede3a1b2e22
Seoa Kim <seoa521@gmail.com>
2023-08-07 18:52:16 +0900

    [feat] 라우터 세팅

---
+++ src/components/Router.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            {" "}
+            {/* Fragment 많은 요소들을 render하고 싶을 때 사용*/}{" "}
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+export default AppRouter;
---
+++ src/routes/Profile.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const EditProfile = () => <span>Profile</span>;
+export default EditProfile;
