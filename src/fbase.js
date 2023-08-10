1c1e369f71202f770a33e676b7ad5ff4f88a6c3c
Seoa Kim <seoa521@gmail.com>
2023-08-10 18:50:47 +0900

    [feat] 트윗 작성, 보여주기 기능 추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseInstance = firebaseConfig;
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,57 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event; // event 안의 tartet의 value를 받아옴
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
