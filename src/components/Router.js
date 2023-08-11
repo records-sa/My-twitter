a4c3231df5026b582f923f54ffbcfa9265209b97
Seoa Kim <seoa521@gmail.com>
2023-08-11 16:39:26 +0900

    [feat] 실시간 트윗 기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
@@ -6,17 +6,16 @@ import Profile from "../routes/Profile";
 import Navigation from "./Navigation";
 import { Redirect } from "react-router-dom";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
       {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
-            {" "}
             {/* Fragment 많은 요소들을 render하고 싶을 때 사용*/}
             <Route exact path="/">
-              <Home />
+              <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile />
