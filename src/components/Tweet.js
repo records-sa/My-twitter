71d972ac2fb32bca16a8f8fa7decabbdc5cf446f
Seoa Kim <seoa521@gmail.com>
2023-08-11 17:50:16 +0900

    [feat] 트윗 수정, 삭제 기능 추가

---
+++ src/components/Tweet.js
@@ -0,0 +1,65 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this tweet?");
+    if (ok) {
+      //delete the tweet
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dbService.doc(`tweets/${tweetObj.id}`).update({
+      text: newTweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTweet(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          {isOwner && (
+            <>
+              <form onSubmit={onSubmit}>
+                <input
+                  type="text"
+                  placeholder="Edit your tweet"
+                  value={newTweet}
+                  required
+                  onChange={onChange}
+                />
+                <input type="submit" value="Update Tweet" />
+              </form>
+              <button onClick={toggleEditing}>Cancel</button>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Tweet;
